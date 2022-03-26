0a7a9de35229bdbe4a4ea1ad064280296fdb5966
Byekibe <kibetpeter95@gmail.com>
2022-03-26 15:44:41 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("Peter");
+  const [mail, setMail] = useState("peter@gmail.com");
+  const [pass, setPass] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const blog = { name, mail, pass };
+
+    console.log(blog);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <br />
+
+        <label>Email:</label>
+        <input
+          type="mail"
+          value={mail}
+          onChange={(e) => setMail(e.target.value)}
+        />
+
+        <br />
+
+        <label>Password: </label>
+        <input
+          type="password"
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+        />
+        <br />
+
+        <button>Submit</button>
+
+        <p>Name: {name}</p>
+        <p>Email: {mail}</p>
+        <p>Password: {pass}</p>
+      </form>
+    </>
   );
 }
 
